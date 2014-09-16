ee603ff10f9b4a354ed3189e444c1bf7ccc559d8
ilz2010 <ilz2010@yandex.ru>
2014-09-16 23:21:48 +0600

    Work with mods and devtools

--- src/main/Logger.java
+++ src/main/Logger.java
@@ -1,27 +1,44 @@
 package main;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class Logger {
- private final ArrayList<LogE> errr = new ArrayList<>();
+ private final ArrayList<LogEx> exep = new ArrayList<>();
  private final ArrayList<LogE> warn = new ArrayList<>();
  private final ArrayList<LogE> info = new ArrayList<>();
  private final ArrayList<LogE> debg = new ArrayList<>();
  
- private final ArrayList<LogEx> ex = new ArrayList<>();
+ 
  public Logger (){
   
  }
  
- public void addE(String clas, String info){
-  errr.add(new LogE(clas,info));
+ public void addE(String clas, Exception ex){
+  this.exep.add(new LogEx(clas,ex));
+ }
+ public void addW(String clas, String info){this.warn.add(new LogE(clas, info));}
+ public void addI(String clas, String info){this.info.add(new LogE(clas, info));}
+ public void addD(String clas, String info){this.debg.add(new LogE(clas, info));}
+  
+ public String getE(){StringBuilder s = new StringBuilder(); exep.stream().forEach(x -> {s.append(x.toString());}); return s.toString();}
+ public String getW(){StringBuilder s = new StringBuilder(); warn.stream().forEach(x -> {s.append(x.toString());}); return s.toString();}
+ public String getI(){StringBuilder s = new StringBuilder(); info.stream().forEach(x -> {s.append(x.toString());}); return s.toString();}
+ public String getD(){StringBuilder s = new StringBuilder(); debg.stream().forEach(x -> {s.append(x.toString());}); return s.toString();}
+ 
+  public void save(){
+  try(ObjectOutputStream s = new ObjectOutputStream(new FileOutputStream(new File("/usr/games/game/log.log")))){
+   s.writeObject(this);
+   s.flush();
+  }catch(IOException e){}
  }
  
- public void addE(String clas, Exception ex){
-  this.ex.add(new LogEx(clas,ex));
- }
+ 
  private class LogE {
   private final String clas;
   private final String info;
@@ -49,7 +66,12 @@ public class Logger {
   }        
   
   @Override public String toString(){
-   return  "[" + new SimpleDateFormat().format(date) + "] "+ clas + " - " + ex+" \n";
+   StringBuilder t = new StringBuilder();
+   
+   for(StackTraceElement e : ex.getStackTrace())
+    t.append(e.toString());
+   
+   return  "[" + new SimpleDateFormat().format(date) + "] "+ clas + " - " + t.toString()+" \n";
   }
  }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,60 +5,60 @@ import mods.basemod.ModContainer;
 import utils.Error;
 import utils.MTimer;
 import utils.Options;
-import utils.exceptions.TermEx; 
+import utils.exceptions.TermEx;
 
-public final class Main implements Runnable{
- public final static String mdir = "/usr/games/game/"; 
+public final class Main implements Runnable {
 
- public final static Options OPTIONS = new Options(mdir + "options.db"); 
- public final static Error ERR_LOG   = new Error();
+ public final static String mdir = "/usr/games/game/";
+
+ public final static Logger LOG = new Logger();
+ public final static Options OPTIONS = new Options(mdir + "options.db");
+ public final static Error ERR_LOG = new Error();
  public final static IdMap IdMap = new IdMap();
  public final static MTimer timer = new MTimer();
  public final static ModContainer mods = new ModContainer();
  public final static CoreModContainer core = new CoreModContainer();
- public final static render.Render rend = /*new render.Render()*/ null;
+ public final static render.Render rend = /*
+          * new render.Render()
+          */ null;
  public final static Main main = new Main();
- public final static Thread Tm = new Thread( main );
+ public final static Thread Tm = new Thread(main);
  //public final static Thread Tr = new Thread( rend );
- 
- 
+
  public boolean running = true;
 
- 
-  
- public void init() throws TermEx, InterruptedException{ 
+ public void init () throws TermEx , InterruptedException {
   mods.loadDir();
+  LOG.addW("Test", "Test2");
+  LOG.save();
   //rend.initfinal();
  }
- 
+
  @Override
- public void run(){
-  try{    
+ public void run () {
+  try {
    init();
 
 //   while (running){  
 //    Tm.wait();
 //   }
-  
    destroy();
-  }catch(TermEx ex){
+  } catch ( TermEx ex ) {
    System.err.println(ex.s);
    Tm.interrupt();
   } catch ( InterruptedException ex ) {
-   
-  } 
+
+  }
  }
-  
 
-
- public void destroy(){
+ public void destroy () {
   ERR_LOG.check();
-  
+
   //level.save();
  }
- 
- public static void main(String[] args){
+
+ public static void main ( String[] args ) {
   Tm.start();
- // Tr.start();
- }   
+  // Tr.start();
+ }
 }
