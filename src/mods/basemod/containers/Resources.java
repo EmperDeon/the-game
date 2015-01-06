65f4908d5ec38cfe897f29b2f533c6bb73e5551f
ilz2010 <ilz2010@yandex.ru>
2015-01-07 00:02:12 +0600

    Started work on Resources(Models and Sounds) and Repository

--- src/mods/basemod/containers/Resources.java
+++ src/mods/basemod/containers/Resources.java
@@ -8,4 +8,20 @@ public class Resources {
  private final TreeMap<Rid, Resource> map = new TreeMap<>();
  
  
+ public Resources(){
+  
+ }
+ 
+ public Resource getResource(Rid id){
+  return map.get(id);
+ }
+ 
+ public void putResource(Rid k, Resource v){
+  this.map.put(k , v);
+ }
+ 
+ public void putAll(){
+  
+ }
+ 
 }
