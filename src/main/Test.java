a17ba91766c829dd65f90f4d3467471173465f48
ilya <ilya@AdminPC>
2015-02-25 13:42:57 +0600

    Work with QtModEditor

--- src/main/Test.java
+++ src/main/Test.java
@@ -1,36 +1,31 @@
 package main;
 
-import com.trolltech.qt.gui.QApplication;
-import main.dev.DevForm;
-import main.dev.LevelEditor;
-import main.dev.ModEditor;
-import main.dev.ModelEditor;
-import main.dev.OptionsEditor;
-
-public class Test {
- public static ModEditor modeditor;
- public static ModelEditor modeleditor;
- public static LevelEditor leveleditor;
- public static OptionsEditor optionseditor;
- public static DevForm devform;
+public class Test extends Thread{
+// public static ModEditor modeditor;
+// public static ModelEditor modeleditor;
+// public static LevelEditor leveleditor;
+// public static OptionsEditor optionseditor;
+// public static DevForm devform;
  public static void main ( String args[] ) {
-
-  QApplication.initialize(args);
-  
-  devform = new DevForm();
-  modeditor = new ModEditor();
-  modeleditor = new ModelEditor();
-  leveleditor = new LevelEditor();
-  optionseditor = new OptionsEditor();
-
-//  devform.show();
-  modeditor.show();
-//  modeleditor.show();
-//  leveleditor.show();
-//  optionseditor.show();
-  
-  QApplication.execStatic();
-  QApplication.shutdown();
-
+//
+//  QApplication.initialize(args);
+//  
+//  devform = new DevForm();
+//  modeditor = new ModEditor();
+//  modeleditor = new ModelEditor();
+//  leveleditor = new LevelEditor();
+//  optionseditor = new OptionsEditor();
+//
+////  devform.show();
+//  modeditor.show();
+////  modeleditor.show();
+////  leveleditor.show();
+////  optionseditor.show();
+//  
+//  QApplication.execStatic();
+//  QApplication.shutdown();
+//
+  System.out.println("/*ds sd*/");
+    
  }
 }
